4a0a98f9c8edde6d463f142ee770aed48d864f8b
Zara Cooper <zramcooper@gmail.com>
2021-06-17 14:30:02 +0300

    Add blog landing page

--- src/app/features/blog/blog-routing.module.ts
+++ src/app/features/blog/blog-routing.module.ts
@@ -1,9 +1,14 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BlogLandingComponent } from './pages/blog-landing/blog-landing.component';
 
 import {BlogComponent} from './pages/blog/blog.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: BlogLandingComponent,
+  },
   {
     path: ':slug',
     component: BlogComponent,
---
+++ src/app/features/blog/pages/blog-landing/blog-landing.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-blog-landing',
+  templateUrl: './blog-landing.component.html',
+  styleUrls: ['./blog-landing.component.css']
+})
+export class BlogLandingComponent implements OnInit {
+  links$: Observable<ScullyRoute[]> = this.scully.available$.pipe(
+    map(routes => routes.filter((route:ScullyRoute) => route.route.startsWith('/blog/')))
+  );
+  
+  constructor(private scully: ScullyRoutesService) { }
+
+  ngOnInit(): void {
+  }
+
+}
