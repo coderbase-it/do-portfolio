dc2256098d2609f1ecb85ef1e0f9b44656a8b686
Zara Cooper <zramcooper@gmail.com>
2021-06-18 02:25:01 +0300

    Add content to home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { 
-    path: 'portfolio', 
+    path: '', 
     loadChildren: () => import('./features/portfolio/portfolio.module').then(m => m.PortfolioModule) 
   },
   { 
