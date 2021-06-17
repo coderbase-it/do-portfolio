d379b52cc326a127ed703a6bdd3558ad128f99f6
Zara Cooper <zramcooper@gmail.com>
2021-06-17 15:03:22 +0300

    Add portfolio components and pages

--- src/app/features/portfolio/portfolio.module.ts
+++ src/app/features/portfolio/portfolio.module.ts
@@ -3,11 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeadingComponent } from './components/heading/heading.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    HeadingComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ProjectsPageComponent
   ],
   imports: [
     CommonModule,
