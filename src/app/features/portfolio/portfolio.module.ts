2ef7cc2b14d4383d249ca97d996dfb60a3dcde70
Zara Cooper <zramcooper@gmail.com>
2021-06-19 20:41:01 +0300

    Remove heading component

--- src/app/features/portfolio/portfolio.module.ts
+++ src/app/features/portfolio/portfolio.module.ts
@@ -5,7 +5,6 @@ import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { HeadingComponent } from './components/heading/heading.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
@@ -16,7 +15,6 @@ import { ProjectsPageComponent } from './pages/projects-page/projects-page.compo
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    HeadingComponent,
     ProjectsComponent,
     SkillsComponent,
     ProjectsPageComponent
