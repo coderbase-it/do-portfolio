ce2f9dd35f66382fae1c448d93c3764b0220902f
Zara Cooper <zramcooper@gmail.com>
2021-06-19 13:16:02 +0300

    Add post list component

--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -4,9 +4,10 @@ import {ScullyLibModule} from '@scullyio/ng-lib';
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './pages/blog/blog.component';
 import { BlogLandingComponent } from './pages/blog-landing/blog-landing.component';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
-  declarations: [BlogComponent, BlogLandingComponent],
+  declarations: [BlogComponent, BlogLandingComponent, PostListComponent],
   imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
 })
 export class BlogModule {}
