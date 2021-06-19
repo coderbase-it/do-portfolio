a17d88d90b5a69aab37ebd94d95e3492cb802104
Zara Cooper <zramcooper@gmail.com>
2021-06-19 14:37:19 +0300

    Move post list component

--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -4,10 +4,9 @@ import {ScullyLibModule} from '@scullyio/ng-lib';
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './pages/blog/blog.component';
 import { BlogLandingComponent } from './pages/blog-landing/blog-landing.component';
-import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
-  declarations: [BlogComponent, BlogLandingComponent, PostListComponent],
+  declarations: [BlogComponent, BlogLandingComponent],
   imports: [CommonModule, BlogRoutingModule, ScullyLibModule],
 })
 export class BlogModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PostListComponent } from './components/post-list/post-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostListComponent
+  ],
   imports: [
     CommonModule,
     NgbModule
   ],
   exports: [
     CommonModule,
-    NgbModule
+    NgbModule, 
+    PostListComponent
   ]
 })
 export class SharedModule { }
