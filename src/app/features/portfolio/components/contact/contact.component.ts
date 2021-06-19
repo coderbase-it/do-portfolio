35450576efed5278643059d9b2ba0fa76aa791c6
Zara Cooper <zramcooper@gmail.com>
2021-06-19 16:51:49 +0300

    Add content to contact component

--- src/app/features/portfolio/components/contact/contact.component.ts
+++ src/app/features/portfolio/components/contact/contact.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  fields = [
+    { type: 'text', placeholder: 'Name', iconClasses: 'fas fa-user-circle', isInput: true },
+    { type: 'email', placeholder: 'Email', iconClasses: 'fas fa-at', isInput: true },
+    { type: 'text', placeholder: 'Subject', iconClasses: 'fas fa-comment-alt', isInput: true },
+    { placeholder: 'Message', iconClasses: 'fas fa-envelope', isTextArea: true },
+  ];
 
   constructor() { }
 
