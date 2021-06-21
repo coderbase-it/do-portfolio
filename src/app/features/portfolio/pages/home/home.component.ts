6f48f823427f14e7da9abffb816be37b07edee15
Zara Cooper <zramcooper@gmail.com>
2021-06-21 17:53:55 +0300

    Get home component data from service

--- src/app/features/portfolio/pages/home/home.component.ts
+++ src/app/features/portfolio/pages/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { share } from 'rxjs/operators';
+import { BioService } from 'src/app/data/services/bio.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  bio$ = this.bioService.getBio().pipe();
 
-  constructor() { }
+  bioOptions = [
+    { viewClasses: 'd-none d-md-flex', headingClass: 'display-3', useH5: false },
+    { viewClasses: 'd-sm-flex d-md-none', headingClass: '', useH5: true }
+  ];
 
-  ngOnInit(): void {
-  }
-
+  constructor(private bioService: BioService) { }
 }
