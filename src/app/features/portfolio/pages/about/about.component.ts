a5bc8a31cb4cd0b2a8e1a0e10bf1caaba961ef16
Zara Cooper <zramcooper@gmail.com>
2021-06-21 18:02:55 +0300

    Add about content from service

--- src/app/features/portfolio/pages/about/about.component.ts
+++ src/app/features/portfolio/pages/about/about.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { BioService } from 'src/app/data/services/bio.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
+  bio$ = this.bioService.getBio().pipe();
 
-  constructor() { }
+  aboutOptions = [
+    { viewClasses: 'd-none d-md-flex', headingClass: 'display-3', useH5: false },
+    { viewClasses: 'd-sm-flex d-md-none', headingClass: '', useH5: true }
+  ];
 
-  ngOnInit(): void {
-  }
-
+  constructor(private bioService: BioService) { }
 }
