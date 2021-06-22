7c29eb901d9c38614892636cfd44ce80240aa2b0
Zara Cooper <zramcooper@gmail.com>
2021-06-22 18:30:22 +0300

    Remove ng-bootstrap from portfolio module

--- src/app/features/portfolio/portfolio.module.ts
+++ src/app/features/portfolio/portfolio.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { PortfolioRoutingModule } from './portfolio-routing.module';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
@@ -25,7 +23,6 @@ import { ThankYouComponent } from './pages/thank-you/thank-you.component';
   ],
   imports: [
     CommonModule,
-    NgbModule,
     ReactiveFormsModule,
     PortfolioRoutingModule
   ]
