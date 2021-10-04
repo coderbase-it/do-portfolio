c7500681f67dd15339416840228bdd736c6c371f
Zara Cooper <zramcooper@gmail.com>
2021-10-04 20:32:22 +0300

    Remove 404 route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './core/pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./features/portfolio/portfolio.module').then(m => m.PortfolioModule)
-  },
-  {
-    path: 'blog',
-    loadChildren: () => import('./features/blog/blog.module').then(m => m.BlogModule)
-  },
-  {
-    path: '404',
-    component: NotFoundComponent
-  },
-  {
-    path: '**',
-    redirectTo: '404'
   }
+  // {
+  //   path: 'blog',
+  //   loadChildren: () => import('./features/blog/blog.module').then(m => m.BlogModule)
+  // },
 ];
 
 @NgModule({
