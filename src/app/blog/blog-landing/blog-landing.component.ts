2010eb15f97a11844ee7ddfd2d1c2e16cd22994b
Zara Cooper <zramcooper@gmail.com>
2021-10-05 16:51:37 +0300

    Populate blog landing

--- src/app/blog/blog-landing/blog-landing.component.ts
+++ src/app/blog/blog-landing/blog-landing.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-blog-landing',
   templateUrl: './blog-landing.component.html',
   styleUrls: ['./blog-landing.component.css']
 })
-export class BlogLandingComponent implements OnInit {
+export class BlogLandingComponent {
+  links$ = this.scully.available$.pipe(
+    map(routes => routes.filter((route: ScullyRoute) => route.route.startsWith('/blog/')))
+  );
 
-  constructor() { }
+  tempOptions = [
+    { viewClasses: 'd-none d-md-flex', displayInColumn: false, titleClasses: 'display-3' },
+    { viewClasses: 'd-flex d-md-none', displayInColumn: true, titleClasses: '' }
+  ];
 
-  ngOnInit(): void {
-  }
-
+  constructor(private scully: ScullyRoutesService) { }
 }
